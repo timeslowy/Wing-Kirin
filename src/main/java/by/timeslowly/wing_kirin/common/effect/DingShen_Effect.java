70eb7b6ebf56a591242d9e550c28c09792e830f7
Dragon-LinFeng <broken_dragon@163.com>
2026-01-20 01:28:32 +0800

    完成效果`定身`的基础移植

--- src/main/java/by/timeslowly/wing_kirin/common/effect/DingShen_Effect.java
+++ src/main/java/by/timeslowly/wing_kirin/common/effect/DingShen_Effect.java
@@ -1,12 +1,117 @@
 package by.timeslowly.wing_kirin.common.effect;
 
+import by.timeslowly.wing_kirin.Wing_kirin;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.Mth;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 // 定身 药水效果
 // TODO:太多了，见发的文档
 public class DingShen_Effect extends MobEffect {
     public DingShen_Effect(MobEffectCategory category, int color) {
         super(category, color);
+        this.addAttributeModifier(Attributes.ATTACK_SPEED,
+                ResourceLocation.fromNamespaceAndPath(Wing_kirin.MOD_ID, "effect.ding_shen_0"), -1,
+                AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL
+        );
+        this.addAttributeModifier(Attributes.MOVEMENT_SPEED,
+                ResourceLocation.fromNamespaceAndPath(Wing_kirin.MOD_ID, "effect.ding_shen_1"), -1,
+                AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL
+        );
+        this.addAttributeModifier(Attributes.GRAVITY,
+                ResourceLocation.fromNamespaceAndPath(Wing_kirin.MOD_ID, "effect.ding_shen_2"), 0,
+                AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL
+        );
+        this.addAttributeModifier(Attributes.MINING_EFFICIENCY,
+                ResourceLocation.fromNamespaceAndPath(Wing_kirin.MOD_ID, "effect.ding_shen_3"), -1,
+                AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL
+        );
+    }
+
+    @Override
+    public boolean shouldApplyEffectTickThisTick(int duration, int amplifier) {
+        return true;
+    }
+
+    @Override
+    public boolean applyEffectTick(@NotNull LivingEntity entity, int amplifier) {
+        // 阻止移动
+        entity.setDeltaMovement(new Vec3(0, 0, 0));
+
+        // 冷却
+        if (entity instanceof Player _player) {
+            _player.getCooldowns().addCooldown(_player.getMainHandItem().getItem(), 10);
+        }
+
+        // 关闭GUI
+        if (Mth.nextInt(RandomSource.create(), 1, 20) == 1) {
+            if (entity instanceof Player _player)
+                _player.closeContainer();
+        }
+
+        return super.applyEffectTick(entity, amplifier);
+    }
+
+    @Override
+    public void onMobRemoved(@NotNull LivingEntity entity, int amplifier, Entity.@NotNull RemovalReason reason) {
+        super.onMobRemoved(entity, amplifier, reason);
+        if (entity.level() instanceof Level _level) {
+            double x = entity.getX();
+            double y = entity.getY();
+            double z = entity.getZ();
+            if (!_level.isClientSide()) {
+                _level.playSound(null,
+                        BlockPos.containing(x, y, z),
+                        Objects.requireNonNull(BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("block.respawn_anchor.deplete"))),
+                        SoundSource.PLAYERS,
+                        2,
+                        (float) 1.2);
+            } else {
+                _level.playLocalSound(x, y, z,
+                        Objects.requireNonNull(BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("block.respawn_anchor.deplete"))),
+                        SoundSource.PLAYERS,
+                        2,
+                        (float) 1.2,
+                        false);
+            }
+        }
+    }
+
+    public static void onEffectExpired(LivingEntity entity, int amplifier) {
+        if (entity.level() instanceof Level _level) {
+            double x = entity.getX();
+            double y = entity.getY();
+            double z = entity.getZ();
+            if (!_level.isClientSide()) {
+                _level.playSound(null,
+                        BlockPos.containing(x, y, z),
+                        Objects.requireNonNull(BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("block.respawn_anchor.deplete"))),
+                        SoundSource.PLAYERS,
+                        2,
+                        (float) 1.2);
+            } else {
+                _level.playLocalSound(x, y, z,
+                        Objects.requireNonNull(BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.parse("block.respawn_anchor.deplete"))),
+                        SoundSource.PLAYERS,
+                        2,
+                        (float) 1.2,
+                        false);
+            }
+        }
     }
 }
--- src/main/java/by/timeslowly/wing_kirin/registry/WKEffects.java
+++ src/main/java/by/timeslowly/wing_kirin/registry/WKEffects.java
@@ -7,19 +7,46 @@ import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.LivingEntity;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.MobEffectEvent;
 import net.neoforged.neoforge.registries.DeferredRegister;
 // 注册药水效果
+@EventBusSubscriber(modid = Wing_kirin.MOD_ID)
 public class WKEffects {
     public static final DeferredRegister<MobEffect> MOB_EFFECTS = DeferredRegister.create(
             Registries.MOB_EFFECT, Wing_kirin.MOD_ID
     );
 
     public static final Holder<MobEffect> DingShen = MOB_EFFECTS.register("ding_shen",
-            () -> new DingShen_Effect(MobEffectCategory.HARMFUL, -1));
+            () -> new DingShen_Effect(MobEffectCategory.HARMFUL, -256));
 
     public static final Holder<MobEffect> GreatZhengqi = MOB_EFFECTS.register("great_zhengqi",
             () -> new GreatZhengqi_Effect(MobEffectCategory.BENEFICIAL, -1));
+
+    // 药水效果移除行为
+    @SubscribeEvent
+    public static void onEffectRemoved(MobEffectEvent.Remove event) {
+        MobEffectInstance effectInstance = event.getEffectInstance();
+        if (effectInstance != null) {
+            expireEffects(event.getEntity(), effectInstance);
+        }
+    }
+    @SubscribeEvent
+    public static void onEffectExpired(MobEffectEvent.Expired event) {
+        MobEffectInstance effectInstance = event.getEffectInstance();
+        if (effectInstance != null) {
+            expireEffects(event.getEntity(), effectInstance);
+        }
+    }
+    private static void expireEffects(LivingEntity entity, MobEffectInstance effectInstance) {
+        if (effectInstance.getEffect().value() instanceof DingShen_Effect) {
+            DingShen_Effect.onEffectExpired(entity, effectInstance.getAmplifier());
+        }
+    }
 
     public static void register(IEventBus eventBus) {
         MOB_EFFECTS.register(eventBus);
