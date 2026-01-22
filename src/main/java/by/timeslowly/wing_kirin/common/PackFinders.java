95c59463c84d641ecb5dec4db8a64261505528ed
timeslowy <13850842688@163.com>
2026-01-22 19:57:42 +0800

    添加降低解锁难度的可选数据包

---
+++ src/main/java/by/timeslowly/wing_kirin/common/PackFinders.java
@@ -0,0 +1,29 @@
+package by.timeslowly.wing_kirin.common;
+
+import by.timeslowly.wing_kirin.Wing_kirin;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.repository.Pack;
+import net.minecraft.server.packs.repository.PackSource;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.AddPackFindersEvent;
+
+@EventBusSubscriber
+public class PackFinders {
+    public PackFinders() {
+    }
+
+    @SubscribeEvent
+    public static void addPackFinders(AddPackFindersEvent event) {
+        event.addPackFinders(
+                ResourceLocation.fromNamespaceAndPath(Wing_kirin.MOD_ID, "data/wing_kirin/datapacks/low_unlock_condition"),
+                PackType.SERVER_DATA,
+                Component.literal("Wing Kirin - Low Unlock Condition"),
+                PackSource.FEATURE,
+                false,
+                Pack.Position.TOP
+        );
+    }
+}
