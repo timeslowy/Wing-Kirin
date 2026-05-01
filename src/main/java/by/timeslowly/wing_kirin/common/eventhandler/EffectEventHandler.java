157856d2c097a28d56aec72468a590047e9e4b40
timeslowy <13850842688@163.com>
2026-05-01 11:48:55 +0800

    紧急修复服务端无法运行的问题

--- src/main/java/by/timeslowly/wing_kirin/common/eventhandler/EffectEventHandler.java
+++ src/main/java/by/timeslowly/wing_kirin/common/eventhandler/EffectEventHandler.java
@@ -2,18 +2,9 @@ package by.timeslowly.wing_kirin.common.eventhandler;
 
 import by.timeslowly.wing_kirin.Wing_kirin;
 import by.timeslowly.wing_kirin.registry.WKEffects;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.client.event.InputEvent;
-import net.neoforged.neoforge.client.event.RenderGuiLayerEvent;
-import net.neoforged.neoforge.client.gui.VanillaGuiLayers;
 import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
 import org.jetbrains.annotations.NotNull;
 
@@ -38,59 +29,6 @@ public class EffectEventHandler {
 
         if (multiplier != 1.0f) {
             event.setAmount(originalDamage * multiplier);
-        }
-    }
-
-    // 禁用交互
-    @SubscribeEvent
-    public static void onPlayerInteract(InputEvent.@NotNull InteractionKeyMappingTriggered event) {
-        // 1. 获取当前的本地玩家
-        Player player = Minecraft.getInstance().player;
-        // 2. 安全检查：玩家为空则直接返回
-        if (player == null) {
-            return;
-        }
-        // 3. 判断是否拥有效果
-        if (player.hasEffect(WKEffects.DING_SHEN)) {
-            // 提示
-            player.displayClientMessage(Component.translatable("actionbar.wing_kirin.ability.stasis_hex.disable_interaction"),true);
-            event.setCanceled(true);
-        }
-    }
-
-    // 显示快捷栏禁用视觉效果
-    @OnlyIn(Dist.CLIENT)
-    @SubscribeEvent
-    public static void onRenderHotbar(RenderGuiLayerEvent.@NotNull Post event) {
-        // 检查当前渲染的元素是否为快捷栏 (HOTBAR)
-        if (event.getName().equals(VanillaGuiLayers.HOTBAR)) {
-            Player player = Minecraft.getInstance().player;
-            if (player == null) return;
-            // 1. 检查全局HUD是否可见 (响应F1键)
-            if (Minecraft.getInstance().options.hideGui) {
-                return; // F1模式下跳过渲染
-            }
-            // 检查玩家是否拥有“定身”效果
-            if (player.hasEffect(WKEffects.DING_SHEN)) {
-                GuiGraphics guiGraphics = event.getGuiGraphics();
-
-                // 获取屏幕尺寸并计算主快捷栏位置
-                int screenWidth = Minecraft.getInstance().getWindow().getGuiScaledWidth();
-                int screenHeight = Minecraft.getInstance().getWindow().getGuiScaledHeight();
-                int hotbarWidth = 182;
-                int hotbarHeight = 22;
-                int x = (screenWidth - hotbarWidth) / 2;
-                int y = screenHeight - hotbarHeight;
-                // 副手
-                int offhandWidth = 22;
-                int offhandHeight = 22;
-                int offhandX = x - offhandWidth - 8; // 主快捷栏左侧，留出8像素间隔
-
-                // 主快捷栏蒙版
-                guiGraphics.fill(x, y, x + hotbarWidth, y + hotbarHeight, 0xB34D4D4D);
-                // 副手槽位蒙版
-                guiGraphics.fill(offhandX, y, offhandX + offhandWidth, y + offhandHeight, 0xB34D4D4D);
-            }
         }
     }
 }
