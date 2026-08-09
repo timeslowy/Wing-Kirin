02ffd6ecbe01c4828c46604b1ae72bb1e9c73557
timeslowy <13850842688@163.com>
2026-08-09 21:57:16 +0800

    删点冗余

--- src/main/java/by/timeslowly/wing_kirin/registry/WKItems.java
+++ src/main/java/by/timeslowly/wing_kirin/registry/WKItems.java
@@ -9,9 +9,7 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 
 // 注册物品
 public class WKItems {
-    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(
-            Wing_kirin.MOD_ID
-    );
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Wing_kirin.MOD_ID);
     // 金风玉露
     public static final DeferredItem<Item> EmpyreanWine = ITEMS.register("empyrean_wine", EmpyreanWineItem::new);
     // 金钟
@@ -22,30 +20,23 @@ public class WKItems {
     public static final DeferredItem<Item> WingKirinTreat = ITEMS.register("wing_kirin_treat", WingKirinTreatItem::new);
     // 通仙心
     public static final DeferredItem<Item> WingKirinHeart = ITEMS.register("wing_kirin_upgrade", WingKirinHeartItem::new);
-    // （没想好叫什么）
+    // 天仙玉露
     public static final  DeferredItem<Item> EmpyreanEssence = ITEMS.register("empyrean_essence", EmpyreanEssenceItem::new);
 
     // 远古翼麒麟头像（空物品）
-    public static final DeferredItem<Item> WingKirinFullIconItem = ITEMS.register("wing_kirin_full_icon",
-            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> WingKirinFullIconItem = ITEMS.register("wing_kirin_full_icon", () -> new Item(new Item.Properties()));
     // 新生翼麒麟头像（空物品）
-    public static final DeferredItem<Item> WingKirinIconItem = ITEMS.register("wing_kirin_icon",
-            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> WingKirinIconItem = ITEMS.register("wing_kirin_icon", () -> new Item(new Item.Properties()));
     // 浩然正气效果图标（空物品）
-    public static final DeferredItem<Item> GreatZhengqiIconItem = ITEMS.register("great_zhengqi_icon",
-            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> GreatZhengqiIconItem = ITEMS.register("great_zhengqi_icon", () -> new Item(new Item.Properties()));
     // 麒麟之翼图标（空物品）
-    public static final DeferredItem<Item> FlyHigherIconItem = ITEMS.register("fly_higher_icon",
-            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> FlyHigherIconItem = ITEMS.register("fly_higher_icon", () -> new Item(new Item.Properties()));
     // 定身药水效果图标（空物品）
-    public static final DeferredItem<Item> DingshenIconItem = ITEMS.register("dingshen_icon",
-            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> DingshenIconItem = ITEMS.register("dingshen_icon", () -> new Item(new Item.Properties()));
     // 「唯快不破」进度图标（空物品）
-    public static final DeferredItem<Item> UnstoppableSpeedIcon = ITEMS.register("unstoppable_speed_icon",
-            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> UnstoppableSpeedIcon = ITEMS.register("unstoppable_speed_icon", () -> new Item(new Item.Properties()));
     // 「壮士断腕」进度图标（空物品）
-    public static final DeferredItem<Item> SpellBinderIcon = ITEMS.register("spell_binder_icon",
-            () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> SpellBinderIcon = ITEMS.register("spell_binder_icon", () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
