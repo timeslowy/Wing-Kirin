be4db9f8033c35d7566194703e4fd529719b363e
timeslowy <13850842688@163.com>
2026-08-30 16:11:24 +0800

    我就这么说：谁来都恼火，莫要自讨苦吃......

---
+++ src/main/java/by/timeslowly/wing_kirin/WingKirin.java
@@ -0,0 +1,37 @@
+package by.timeslowly.wing_kirin;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(WingKirin.MODID)
+public class WingKirin {
+
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "wing_kirin";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
---
+++ src/main/resources/data/wing_kirin/function/dragon_ability/weather_clear/main.mcfunction
@@ -0,0 +1,9 @@
+## 执行者：玩家
+# 钠，天气要开始放晴了哟~
+weather clear
+
+# TODO:用于触发进度————云开雾散
+# execute if entity @s[advancements={wing_kirin:wing_kirin/weather_clear=false}] run advancement grant @s only wing_kirin:wing_kirin/weather_clear
+
+# TODO:增加“施法云销雨霁次数”统计数据值
+# wk-stats add @s wing_kirin:casted_weather_clear_times 1
