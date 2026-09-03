a774b637844c266ed2d487e74c376582311e94e6
timeslowy <13850842688@163.com>
2026-09-03 16:55:25 +0800

    测试后目前的第一轮进度......将8个测试可行的技能安上，暂时不考虑复杂技能、缺陷和一些图标

--- src/main/java/by/timeslowly/wing_kirin/WingKirin.java
+++ src/main/java/by/timeslowly/wing_kirin/WingKirin.java
@@ -2,13 +2,12 @@ package by.timeslowly.wing_kirin;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModContainer;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
